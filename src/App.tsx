d3b9de1148aaad23ee896251088daf269104f1e8
Ghayoor <ghayoorshan7@gmail.com>
2024-09-09 02:39:37 +0400

    add: tailwind css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <>Restaurant Menu</>;
+  return (
+    <>
+      <div className="text-3xl font-bold underline">Restaurant Menu</div>
+    </>
+  );
 }
 
 export default App;
